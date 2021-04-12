6d850ed1917da42ee98cfdbdeba8407b6adba2e6
DavidHromjak-code <82401817+DavidHromjak-code@users.noreply.github.com>
2021-04-12 18:46:31 +0200

    Add files via upload

---
+++ uloha1/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link type="text/css" href="brikot.css" rel="stylesheet">
+    </head>
+    <body>
+        
+        <div class=" structure" id="structureFP">
+            <div class="header">
+                <div class="nadpis"><h1>Brikot</h1></div>
+                <div class="navbar">
+                    <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="AboutUs.php">AboutUs</a></li>
+  <li><a href="Services.php">Services</a></li>
+  <li><a href="Blog.php">Blog</a></li>
+   <li><a href="Services.php">Services</a></li>
+  <li><a href="Blog.php">Blog</a></li>
+</ul>
+                </div>
+            </div>
+          
+            <div class="contentFP">
+                <hr>
+                <h2>NOVÁ VYUČOVACÍ METODA</h2>
+                <h1>z Kolína</h1>
+                <div class="button"><span>Jak to Probíhá</span></div>
+                
+            </div>
+        </div>
+        
+        <div class="structure" id="structureSP">
+           
+            <div class="contentSP">
+                 <h1> O METODĚ</h1>
+            <p class="contentP">VÝUKOVÝ POSTUP JAK SE STÁT KOKOTEM</p>
+            
+                <div>
+                    <img>
+                    <h2>Plánování</h2>
+                    <p>Nejdříve si musíte vybrat tu nejhnusnější školu v okolí s oborem, který Vás vůbec nebaví. Musíte si vybrat opravdu tu nejhorší, kterou můžete najít. Poté se na ní musíte dostat, to je zákon.</p>
+                </div>
+                <div>
+                    <img>
+                    <h2>Vývoj</h2>
+                    <p>Pokud se úspěšně dostanete na tu nejhorší školu, musíte ji pravidelně navštěvovat a chodit na všechny hodiny. Nesmíte zapomínat nadávat na předměty, učitele a také na jídlo ve školní jídelně i když tam vůbec nechodíte.</p>
+                </div>
+                <div>
+                    <img>
+                    <h2>Dosažení</h2>
+                    <p>Pokud úspěšně zdoláte všechny roky studia a ani jednou jste na škole nebyli štastni, a pořád jste jen nadávali, tak vám gratulejeme, stali jste se totiž tím největším kokotem ze všech.</p>
+                </div>
+            </div>
+            
+        </div>
+        <div class="structure" id="structureTP">
+           
+            <div class="contentTP">
+                  <h1> O METODĚ</h1>
+            <p class="contentP">VÝUKOVÝ POSTUP JAK SE STÁT KOKOTEM</p>
+            
+            <div>
+                <img src="obrazky/team1.jpg" alt="team1"/>
+                <p>Dobrohněva</p>
+                <h3>Pornoherečka</h3>
+            </div>
+            <div>
+                <img src="obrazky/team2.jpg" alt="team2"/>
+                <p>Hrozislava</p>
+                <h3>Komunistka</h3>
+            </div>
+            <div>
+                <img src="obrazky/team3.jpg" alt="team3"/>
+                <p>Lucina</p>
+                <h3>Otrokyně</h3>
+            </div>
+            </div>
+            <div class="structure" id="structure4P">
+               
+                <div class="content4P">
+                     <h1>KONTAKTUJTE NÁS</h1>
+                <p class="contentP"> METODA BRIKOT CHCE PRÁVĚ VÁS</p>
+                <div class="odsazeni">
+                        <img>
+                        <p class="contentP">666-646-6666</p>
+                    </div>
+                    <div>
+                        <img>
+                        <p class="contentP">info@brikot.cz</p>
+                    </div>
+                    <div>
+                        <img>
+                        <p class="contentP">Jaselská 826, Kolín</p>
+                    </div>
+                   <form class="">
+
+<!-- Text input-->
+
+  <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+ 
+ <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+ 
+  <input id="textinput" name="textinput" type="text" placeholder="placeholder" class="form-control input-md">
+<!-- Textarea -->
+           
+    <textarea class="form-control" id="textarea" name="textarea">default text</textarea>
+
+
+
+<!-- Button -->
+
+    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Button</button>
+
+</form>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="footer">
+            <p>Copyright © 2019 Metoda Brikot | "David Hromják"</p>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
